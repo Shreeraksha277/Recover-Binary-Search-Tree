68729b190b609064242da45c01e529fdf8e7084f
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-05-06 19:31:09 +0530

    Create sol.java

---
+++ sol.java
@@ -0,0 +1,31 @@
+class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+
+        // swap values
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+
+        inorder(root.left);
+
+        // detect violation
+        if (prev.val > root.val) {
+            if (first == null) {
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
+
+        inorder(root.right);
+    }
+}
